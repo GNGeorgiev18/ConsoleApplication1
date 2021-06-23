1a139a900be95d2b8c1d3a2d0413274939f624d6
GNGeorgiev18 <58163283+GNGeorgiev18@users.noreply.github.com>
2021-06-23 19:55:00 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
 using namespace std;
 
 struct ROOM_PRICING
 {
-    int singleRoom = 50;
-    int doubleRoom = 90;
-    int maisonetteRoom = 135;
+    int singleRoom;
+    int doubleRoom;
+    int maisonetteRoom;
 };
 
 struct REGISTER
@@ -23,7 +22,7 @@ class Motel
 {
     string name;
     int capacity = 10;
-    vector<REGISTER> bookings = {};
+    vector<REGISTER> bookings;
     unsigned int currentBookingNumber = 0;
     int singleRoomCount = capacity * (40.0 / 100.0);
     int doubleRoomCount = capacity * (40.0 / 100.0);
@@ -33,25 +32,33 @@ class Motel
 public:
     Motel(string name, int capacity)
     {
-
+        name = name;
+        capacity = capacity;
     }
-    /*ROOM_PRICING getRoomPricing()
-    {
 
-    }*/
+    ROOM_PRICING getRoomPricing()
+    {
+        ROOM_PRICING room;
+        room.singleRoom = 50;
+        room.doubleRoom = 90;
+        room.maisonetteRoom = 135;
+        return room;
+    }
 
     string rentRoom(string clientName, string roomType, int numberOfNights)
     {
+        REGISTER temp;
+        temp.clientName = clientName;
+        temp.roomType = roomType;
+        temp.nights = numberOfNights;
         if (roomType == "singleRoom")
         {
             if (singleRoomCount > 0)
             {
                 i++;
                 rentNumber++;
-                bookings.push_back({ bookings[i].clientName = clientName });
-                bookings.push_back({ bookings[i].roomType = roomType });
-                //bookings.push_back({ bookings[i].nights = numberOfNights });
-                //bookings.push_back({ bookings[i].rentNumber = rentNumber });
+                temp.rentNumber = rentNumber;
+                bookings.push_back(temp);
                 singleRoomCount--;
                 return "Enjoy your time here Mr. / Mrs. " + clientName + ". Your booking is " + to_string(rentNumber) + ".";
             }
@@ -91,10 +98,31 @@ public:
         }
         return  "Not happening";
     }
+
+    string checkOut(int bookingNumber)
+    {
+        if (bookings[bookingNumber-1].roomType == "singleRoom")
+        {
+            singleRoomCount++;
+            return "We hope you enjoyed your time here, Mr. / Mrs. " + bookings[bookingNumber - 1].clientName + ". The total amount of money you have to pay is " + to_string(bookings[bookingNumber - 1].nights * 50) + " BGN.";
+        }
+        if (bookings[i].roomType == "doubleRoom")
+        {
+            doubleRoomCount++;
+            return "We hope you enjoyed your time here, Mr. / Mrs. " + bookings[bookingNumber - 1].clientName + ". The total amount of money you have to pay is " + to_string(bookings[bookingNumber - 1].nights * 90) + " BGN.";
+        }
+        if (bookings[i].roomType == "maisonetteRoom")
+        {
+            mainsonetteRoomCount++;
+            return "We hope you enjoyed your time here, Mr. / Mrs. " + bookings[bookingNumber - 1].clientName + ". The total amount of money you have to pay is " + to_string(bookings[bookingNumber - 1].nights * 135) + " BGN.";
+        }
+        return "The booking " + to_string(bookingNumber) + "is invalid.";
+    }
 };
 
 int main()
 {
-    Motel m("ssd", 10);
-    cout << m.rentRoom("Ivan", "singleRoom", 3);
+    Motel motel = Motel("MedenRudnik", 10);
+    cout << motel.rentRoom("Peter", "singleRoom", 3);
+    cout << motel.rentRoom("Peter", "singleRoom", 3);
 }
